e0c4c884ab3ce676b337ef79ed3c7b9f7d527db3
KingO'Cacti <102800451+J-Riby-2003@users.noreply.github.com>
2024-03-27 17:39:44 +0000

    Initial Commit
    Initial file commit to repo.

---
+++ Practice/react-app/src/App.tsx
@@ -0,0 +1,13 @@
+import Alert from "./components/Alert";
+
+function App() {
+  return (
+    <div>
+      <Alert>
+        Hello <span>James!!</span>
+      </Alert>
+    </div>
+  );
+}
+
+export default App;
